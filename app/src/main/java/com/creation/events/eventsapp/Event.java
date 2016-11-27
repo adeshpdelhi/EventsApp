cbd353f8cd2145e3b9772a3c1ae08f85beb96ffc
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-11-27 20:40:05 +0530

    failed try of retrofit 2.1.. now trying 1.9 next

--- app/src/main/java/com/creation/events/eventsapp/Event.java
+++ app/src/main/java/com/creation/events/eventsapp/Event.java
@@ -13,19 +13,75 @@ public class Event implements Serializable {
 
     public String name;
     public String date;
-   public String time_from;
-   public String time_to;
+    public String time_from;
+    public String time_to;
     public String club;
     public String description;
     public String organisers;
 
-    public Event(String name,String club,String date,String time_from,String time_to,String description,String organisers)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime_from() {
+        return time_from;
+    }
+
+    public void setTime_from(String time_from) {
+        this.time_from = time_from;
+    }
+
+    public String getTime_to() {
+        return time_to;
+    }
+
+    public void setTime_to(String time_to) {
+        this.time_to = time_to;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public void setClub(String club) {
+        this.club = club;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOrganisers() {
+        return organisers;
+    }
+
+    public void setOrganisers(String organisers) {
+        this.organisers = organisers;
+    }
+
+    public Event(String name, String club, String date, String time_from, String time_to, String description, String organisers)
     {
         this.name=name;
         this.club=club;
         this.date=date;
-       this.time_from=time_from;
-       this.time_to=time_to;
+        this.time_from=time_from;
+        this.time_to=time_to;
         this.description=description;
         this.organisers=organisers;
     }
--- app/src/main/java/com/creation/events/eventsapp/HomeActivity.java
+++ app/src/main/java/com/creation/events/eventsapp/HomeActivity.java
@@ -1,12 +1,10 @@
 package com.creation.events.eventsapp;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -17,24 +15,30 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-
 import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
-
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 /**
  * Created by Rishabh on 10/15/2016.
  */
 public class HomeActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
     public static final String TAG= "HomeActivity";
+    private static final String ROOT_URL = "http://192.168.0.104:3000/";
     GoogleApiClient mGoogleApiClient;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,12 +54,12 @@ public class HomeActivity extends AppCompatActivity implements GoogleApiClient.O
         User user = new User(sharedPref.getString("username",null),sharedPref.getString("email",null));
        // Log.v(TAG, "Username is "+user.getName().toString());
         getSupportActionBar().setTitle("Events");
-        populateEventsList();
+        fetchAndUpdateList();
     }
     @Override
     protected  void onResume(){
         super.onResume();
-        populateEventsList();
+        fetchAndUpdateList();
     }
 
 
@@ -89,11 +93,52 @@ public class HomeActivity extends AppCompatActivity implements GoogleApiClient.O
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
     }
 
-    private void populateEventsList() {
+    private void fetchAndUpdateList(){
+        //While the app fetched data we are displaying a progress dialog
+        final ProgressDialog loading = ProgressDialog.show(this,"Fetching Data","Please wait...",false,false);
+
+        //Creating a rest adapter
+        Retrofit adapter = new Retrofit.Builder()
+                .baseUrl(ROOT_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //Creating an object of our api interface
+        EventAPI api = adapter.create(EventAPI.class);
+
+        Call<List<Event>> call = api.getEvents();
+        call.enqueue(new Callback<List<Event>>() {
+            @Override
+            public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
+                // response.isSuccessful() is true if the response code is 2xx
+                if (response.isSuccessful()) {
+                    Log.v(TAG, response.body().toString());
+                    updateListView(new ArrayList<Event>(response.body()));
+                } else {
+                    int statusCode = response.code();
+
+                    // handle request errors yourself
+                    Log.e(TAG,response.toString());
+                    Toast.makeText(getApplicationContext(),"Error!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Event>> call, Throwable t) {
+                // handle execution failures like no internet connectivity
+                Log.e(TAG,t.toString());
+                Toast.makeText(getApplicationContext(),"Internet Error!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateListView(ArrayList<Event> list) {
         // Construct the data source
-        EventDB mydb=new EventDB(this);
-        ArrayList<Event> arrayOfEvents = mydb.getAllEvents();
+//        EventDB mydb=new EventDB(this);
+//        ArrayList<Event> arrayOfEvents = mydb.getAllEvents();
         // Create the adapter to convert the array to views
+        ArrayList<Event> arrayOfEvents = list;
+
         EventsAdapter adapter = new EventsAdapter(this, arrayOfEvents);
         // Attach the adapter to a ListView
         ListView listView = (ListView) findViewById(R.id.list);
@@ -126,7 +171,7 @@ public class HomeActivity extends AppCompatActivity implements GoogleApiClient.O
     {
         Intent intent = new Intent(this, AddEventActivity.class);
         startActivity(intent);
-        populateEventsList();
+        fetchAndUpdateList();
     }
     public void signOut() {
         Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
